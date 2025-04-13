5051cd78e3c99f1d66f3f9f146e0cad6b38d8d3a
mikezn <mikezn@gmail.com>
2025-04-13 07:41:15 +0700

    v0.9 ----  - detect bird -> action: duck  - detect game over
     FUTURE IMPROVEMENTS
    ----
     - not 100% accurate, sometimes does not jump
      - poossibly when two obstacles are close together, dino detects first and jumps, but is still in the air when the next obstacle is detected so doesn't register a jump in time.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import pyautogui
 import PIL
 import sys
 
-WATCH_PIXEL_POS = (390, 240)
-GAME_OVER_PIXEL_1 = (525, 325)
-GAME_OVER_PIXEL_2 = (548, 325)
+WATCH_CACTUS_PIXEL_POS = (390, 240)
+WATCH_BIRD_PIXEL_POS = (390, 207)
+GAME_OVER_PIXEL_1 = (470, 200)
+GAME_OVER_PIXEL_2 = (497, 225)
 WINDOW_TITLE = 'dino game'
 pixel_main = None
 
@@ -33,20 +34,30 @@ def main() -> None:
     if not game_ready():
         sys.exit()
 
-    pixel_main = pyautogui.pixel(WATCH_PIXEL_POS[0] + get_window_loc()[0], WATCH_PIXEL_POS[1] + get_window_loc()[1])
+    pixel_main = pyautogui.pixel(WATCH_CACTUS_PIXEL_POS[0] + get_window_loc()[0], WATCH_CACTUS_PIXEL_POS[1] + get_window_loc()[1])
+    pyautogui.press("space")
     while True:
 
-        pixel_compare = pyautogui.pixel(WATCH_PIXEL_POS[0] + get_window_loc()[0], WATCH_PIXEL_POS[1] + get_window_loc()[1])
+        pixel_cactus_compare = pyautogui.pixel(WATCH_CACTUS_PIXEL_POS[0] + get_window_loc()[0], WATCH_CACTUS_PIXEL_POS[1] + get_window_loc()[1])
+        pixel_bird_compare = pyautogui.pixel(WATCH_BIRD_PIXEL_POS[0] + get_window_loc()[0], WATCH_BIRD_PIXEL_POS[1] + get_window_loc()[1])
         pixel_game_over_1 = pyautogui.pixel(GAME_OVER_PIXEL_1[0] + get_window_loc()[0], GAME_OVER_PIXEL_1[1] + get_window_loc()[1])
         pixel_game_over_2 = pyautogui.pixel(GAME_OVER_PIXEL_2[0] + get_window_loc()[0], GAME_OVER_PIXEL_2[1] + get_window_loc()[1])
         print(pixel_main)
-        print(pixel_compare)
-        print(WATCH_PIXEL_POS[0] + get_window_loc()[0], WATCH_PIXEL_POS[1] + get_window_loc()[1])
+        print(pixel_cactus_compare)
+        print(get_window_loc()[0], get_window_loc()[1])
 
-        if pixel_main != pixel_compare:
+        # detect cactus
+        if pixel_main != pixel_cactus_compare:
             pyautogui.press("space")
-            print("DIFFERENT")
-            print(pixel_compare)
+            print("CACTUS")
+            print(pixel_cactus_compare)
+
+        # detect bird
+        if pixel_main != pixel_bird_compare:
+            pyautogui.press("down")
+            print("BIRD")
+            print(pixel_bird_compare)
+
 
         if pixel_game_over_1 != pixel_main and pixel_game_over_2 != pixel_main:
             print("GAME OVER")
